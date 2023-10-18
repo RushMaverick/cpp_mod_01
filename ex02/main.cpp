eb807223501f8d09cba5b8a4d17bbbaf414ec7c5
Rasmus Rask <rasse.rask@gmail.com>
2023-10-18 12:42:40 +0300

    Ex02 done

---
+++ ex02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void){
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "Mem address of str: " << &str << std::endl;
+	std::cout << "Mem address of stringPTR: << " << stringPTR << std::endl;
+	std::cout << "Mem address of stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of stringREF: " << stringREF << std::endl;
+	return 0;
+}
