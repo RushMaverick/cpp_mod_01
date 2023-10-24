e028bdd9f53ccac8330362ea9dc517b2f08481bb
Rasmus Rask <rasse.rask@gmail.com>
2023-10-24 08:05:25 +0300

    Somehow it works

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -5,24 +5,31 @@ int main (int argc, char **argv) {
 		std::cout << "Too few arguments. >> (<filename>, stringToReplace, stringToReplaceWith)" << std::endl;
 		return 0;
 	}
-	// Open argv[1] using std::ifstream inFile(argv[1])
-	// if inFile fails, tell user to fuck off
 	std::ifstream inFile(argv[1]);
 	if (!inFile.is_open()) {
 		std::cout << "Error opening file, try another one." << std::endl;
 		return 1;
 	}
-	std::string outFileName = std::string(argv[1]) + ".replace";
-	std::ofstream outFile(outFileName);
 	std::string line;
 	std::stringstream buffer;
+	size_t pos = 0;
+	size_t index = 0;
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+	std::string outFileName = std::string(argv[1]) + ".replace";
+	std::ofstream outFile(outFileName);
 	buffer << inFile.rdbuf();
 
 	line = buffer.str();
 
-	for (size_t i = 0; i < line.size(); i++) {
-		outFile << line[i];
+	while((index = line.find(s1, pos)) != std::string::npos) {
+		line.erase(index, s1.length());
+		line.insert(index, s2);
+		pos = index + s2.length();
 	}
+	outFile << line << std::endl;
+	outFile.close();
+	inFile.close();
 	return 0;
 }
 
