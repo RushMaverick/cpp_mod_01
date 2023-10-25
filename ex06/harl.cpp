581e7b4f891d03fdf631c538c04bd6932f606ad9
Rasmus Rask <rasse.rask@gmail.com>
2023-10-25 19:05:43 +0300

    ex06 still needs to execute the other commands as well.

---
+++ ex06/harl.cpp
@@ -0,0 +1,65 @@
+#include "harl.hpp"
+
+void Harl::_debug(void) {
+	std::cout
+	<< "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!"
+	<< std::endl;
+}
+
+void Harl::_info(void) {
+	std::cout
+	<< "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+	<< std::endl;
+}
+void Harl::_warning(void) {
+	std::cout
+	<< "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." 
+		<< std::endl;
+}
+void Harl::_error(void) {
+	std::cout << 
+		"This is unacceptable! I want to speak to the manager now."
+		<< std::endl;
+}
+
+Harl::Harl() {
+}
+
+Harl::~Harl() {
+
+}
+
+void Harl::complain(std::string msg) {
+
+	switch ((this->_enumerator(msg))) {
+		case DEBUG:{
+			this->_debug();
+			break;
+		}
+		case INFO:{
+			this->_info();
+			break;
+		}
+		case WARNING:{
+			this->_warning();
+			break;
+		}
+		case ERROR:{
+			this->_error();
+			break;
+		}
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
+}
+
+int Harl::_enumerator (std::string msg) {
+	std::string lvls[4] = {"debug", "info", "warning", "error"};
+
+	for (int i = 0; i < 4; i++) {
+		if ((msg.compare(lvls[i])) == 0) {
+			return (i);
+		}
+	}
+	return (-1);
+}
