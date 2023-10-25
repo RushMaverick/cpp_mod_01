80aad9b3d3f8682247b3132eac26ddb374c158d1
Rasmus Rask <rasse.rask@gmail.com>
2023-10-25 19:09:12 +0300

    lol 5 min fix

--- ex06/harl.cpp
+++ ex06/harl.cpp
@@ -2,23 +2,23 @@
 
 void Harl::_debug(void) {
 	std::cout
-	<< "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!"
+	<< "[DEBUG] I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!"
 	<< std::endl;
 }
 
 void Harl::_info(void) {
 	std::cout
-	<< "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+	<< "[INFO] I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
 	<< std::endl;
 }
 void Harl::_warning(void) {
 	std::cout
-	<< "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." 
+	<< "[WARNING] I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." 
 		<< std::endl;
 }
 void Harl::_error(void) {
 	std::cout << 
-		"This is unacceptable! I want to speak to the manager now."
+		"[ERROR] This is unacceptable! I want to speak to the manager now."
 		<< std::endl;
 }
 
@@ -34,15 +34,12 @@ void Harl::complain(std::string msg) {
 	switch ((this->_enumerator(msg))) {
 		case DEBUG:{
 			this->_debug();
-			break;
 		}
 		case INFO:{
 			this->_info();
-			break;
 		}
 		case WARNING:{
 			this->_warning();
-			break;
 		}
 		case ERROR:{
 			this->_error();
